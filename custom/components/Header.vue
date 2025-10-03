10a9ac6966fdadd2faabb604d4a2173800d6a5d9
jonathan-martz <github@jmartz.de>
2025-10-03 19:24:56 +0200

    start with daisuyi config, owrk on header and index.vue

--- custom/components/Header.vue
+++ custom/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="py-3 px-3">
-    <section class="bg-white rounded-xl px-3 py-3 flex justify-between">
+  <div class="py-3 px-3 mx-auto max-w-6xl">
+    <section class="bg-red-400 rounded-xl px-3 py-3 flex justify-between">
       <section class="logo">
         <a href="/" class="font-bold">Pocketstore.io - Plugins</a>
       </section>
@@ -14,8 +14,15 @@
         </button>
       </nav>
     </section>
-    <section v-if="open" class="navigation mt-3 mobile md:hidden bg-white rounded-xl px-3 py-3">
-      Navigation
+    <section v-if="open" class="navigation mt-3 mobile lg:hidden bg-white rounded-xl px-3 py-3">
+      <section class="grid grid-cols-6 gap-3">
+        <div class="col-span-3">
+          <a href="/de" class="btn btn-block btn-primary">Startseite</a>
+        </div>
+        <div class="col-span-3">
+          <a href="/de/catalog" class="btn btn-block btn-primary">Katalog</a>
+        </div>
+      </section>
     </section>
   </div>
 </template>
@@ -23,9 +30,9 @@
 <script setup lang="ts">
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 import {useLocalStorage} from '@vueuse/core'
-import {onMounted} from "../../storefront/.nuxt/imports";
 
 const open = useLocalStorage('navigation', false)
+
 onMounted(()=>{
   open.value = false;
 })
--- custom/pages/index.vue
+++ custom/pages/index.vue
@@ -1,5 +1,16 @@
 <template>
   <section class="page bg-white px-3 py-3">
-    Hallo Welt
+    <section class="grid grid-cols-6 gap-3">
+      <div class="col-span-6 md:col-span-2">
+        Filter (Name)
+      </div><div class="col-span-6 md:col-span-2">
+        Filter (Category)
+      </div><div class="col-span-6 md:col-span-2">
+        Filter (Price)
+      </div>
+      <div class="col-span-6 md:col-span-2">
+        Product Cards
+      </div>
+    </section>
   </section>
 </template>
