6cb4cca9d08fdf837f21600e3daaa598cb6040ca
jonathan-martz <github@jmartz.de>
2025-10-03 21:00:43 +0200

    work on layout catalog and index page

--- custom/pages/index.vue
+++ custom/pages/index.vue
@@ -1,18 +1,71 @@
 <template>
   <section class="page bg-white px-3 py-3">
     <section class="grid grid-cols-6 gap-3">
-      <div class="col-span-6 md:col-span-2">
-        Filter (Name)
+      <div class="col-span-6">
+        <h2 class="font-bold text-lg">Willkommen in unserem PocketStore für Plugins von PocketStore</h2>
+        <p class="block text-left max-w-xl">
+          Hier findest du alle Plugins wie die Kasse, den Katalog und alle andere Plugins.
+          In Zukunft kommen auch custom plugins von anderen Herstellern.
+        </p>
       </div>
-      <div class="col-span-6 md:col-span-2">
-        Filter (Category)
+      <div class="col-span-6 md:col-span-3">
+        <div class="hero bg-gray-300 py-12">
+          <div class="hero-content text-center">
+            <div class="max-w-md">
+              <h1 class="text-5xl font-bold">Hello there</h1>
+              <p class="py-6">
+                Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                quasi. In deleniti eaque aut repudiandae et a id nisi.
+              </p>
+              <button class="btn btn-primary">Get Started</button>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="col-span-6 md:col-span-2">
-        Filter (Price)
+      <div class="col-span-6 md:col-span-3">
+        <div class="hero bg-gray-300 py-12">
+          <div class="hero-content text-center">
+            <div class="max-w-md">
+              <h1 class="text-5xl font-bold">Hello there</h1>
+              <p class="py-6">
+                Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                quasi. In deleniti eaque aut repudiandae et a id nisi.
+              </p>
+              <button class="btn btn-primary">Get Started</button>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="col-span-6 md:col-span-2">
-        Product Cards
+      <div class="col-span-6 md:col-span-3">
+        <div class="hero bg-gray-300 py-12">
+          <div class="hero-content text-center">
+            <div class="max-w-md">
+              <h1 class="text-5xl font-bold">Hello there</h1>
+              <p class="py-6">
+                Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                quasi. In deleniti eaque aut repudiandae et a id nisi.
+              </p>
+              <button class="btn btn-primary">Get Started</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-span-6 md:col-span-3">
+        <div class="hero bg-gray-300 py-12">
+          <div class="hero-content text-center">
+            <div class="max-w-md">
+              <h1 class="text-5xl font-bold">Hello there</h1>
+              <p class="py-6">
+                Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                quasi. In deleniti eaque aut repudiandae et a id nisi.
+              </p>
+              <button class="btn btn-primary">Get Started</button>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   </section>
 </template>
+<script setup lang="ts">
+</script>
